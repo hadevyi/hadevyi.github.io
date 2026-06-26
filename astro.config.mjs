4714f1f9b7b3b0cba82e5bbd9f998c7d28205c48
hadevyi <hadevyi@gmail.com>
2026-06-26 13:19:13 +0900

    build: 메인페이지 사이트맵 설정 추가 (hadevyi.github.io#45)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap()]
 });
